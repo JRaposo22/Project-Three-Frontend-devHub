f85f66b7b897c1135ea36fae60e2f652fe267e60
Joao <raposo2281@gmail.com>
2023-03-10 16:28:50 +0000

    adds quiz frontend

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,28 +1,14 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
-import axios from 'axios';
+import { Link } from 'react-router-dom';
 import quizService from '../services/quiz.service';
 
 function Quiz() {
-
-    const {type} = useParams();
-
-    const getQuiz = async () => {
-        try {
-            const response = quizService.getRandomQuiz();
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
   return (
-    <div>
-        <h1>{type} Quiz</h1>
-        {response.map((question) => {
-            return (
-                <h2>{question.question}</h2>
-            )
-        })}
+    <div> 
+    <h1>All quizes</h1>
+      <Link to='/quiz/general'>General Quiz</Link>
+      <Link to={`/quiz/javscript`}>Javascritp Quiz</Link>
+      <Link to={`/quiz/html`}>HTML Quiz</Link>
     </div>
   )
 }
---
+++ src/pages/QuizType.jsx
@@ -0,0 +1,43 @@
+import React, {useState, useEffect} from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import quizService from '../services/quiz.service';
+
+function QuizType() {
+
+    const [quiz, setQuiz] = useState([])
+    const {type} = useParams();
+    console.log(type)
+    console.log('TESTE')
+
+    const getQuiz = async () => {
+        try {
+            const response = await quizService.getRandomQuiz();
+            setQuiz(response.data);
+            console.log(response.data)
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        getQuiz();
+    }, [])
+
+  return (
+    <div>
+    <h1>QUIZ</h1>
+        <h1>{type} Quiz</h1>
+        {quiz.map((question) => {
+            return (
+                <div key={question._id}>
+                <h2>{question.question}</h2>
+                </div>
+                
+            )
+        })}
+    </div>
+  )
+}
+
+export default QuizType
