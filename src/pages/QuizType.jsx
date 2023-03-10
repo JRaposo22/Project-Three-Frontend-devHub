85a395e33b88fd19d75f8aa1812c82a8898c3a75
Joao <raposo2281@gmail.com>
2023-03-10 18:22:58 +0000

    quiz with next button

--- src/pages/QuizType.jsx
+++ src/pages/QuizType.jsx
@@ -5,12 +5,14 @@ import quizService from '../services/quiz.service';
 
 function QuizType() {
 
+    const [qNumber, setQNumber] = useState(0)
     const [quiz, setQuiz] = useState([])
     const {type} = useParams();
     console.log(type)
     console.log('TESTE')
 
     const getQuiz = async () => {
+        
         try {
             const response = await quizService.getRandomQuiz(type);
             setQuiz(response.data);
@@ -27,18 +29,16 @@ function QuizType() {
   return (
     <div>
         <h1>{type} Quiz</h1>
-        {quiz.map((question) => {
-            return (
-                <div key={question._id}>
-                <h2>{question.question}</h2>
-                {question.answers.map((answer, i) => {
-                    return <h4 key={i}>{answer}</h4>
-                })}
-
-                </div>
-                
-            )
-        })}
+         {(quiz.length > 0) && (
+            <>
+            <h2>{quiz[qNumber].question}</h2> 
+            {quiz[qNumber].answers.map((answer, i) => {
+                return <h4 key={i}>{answer}</h4>
+            })} 
+            </>
+           
+        )} 
+        <button onClick={() => {setQNumber(qNumber + 1)}}>Next</button>      
     </div>
   )
 }
