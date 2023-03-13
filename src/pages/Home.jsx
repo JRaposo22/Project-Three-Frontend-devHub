7a39a60a3655ab4eff9d7b4bb7ae1b8c802bcf77
Joao <raposo2281@gmail.com>
2023-03-13 12:50:52 +0000

    finished quiz

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+
 function Home() {
   return (
     <div>
       <Link to='/signup'>Signup</Link>
+      <Link to='/quiz'>Quiz</Link>
     </div>
   )
 }
--- src/pages/QuizType.jsx
+++ src/pages/QuizType.jsx
@@ -2,25 +2,57 @@ import React, {useState, useEffect} from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import quizService from '../services/quiz.service';
+import './QuizType.css'
+
 
 function QuizType() {
 
     const [qNumber, setQNumber] = useState(0)
     const [quiz, setQuiz] = useState([])
+    const [quizEnd, setQuizEnd] = useState(false);
     const {type} = useParams();
-    console.log(type)
-    console.log('TESTE')
+    
+    
 
     const getQuiz = async () => {
         
         try {
             const response = await quizService.getRandomQuiz(type);
             setQuiz(response.data);
-            console.log(response.data)
+
         } catch (error) {
             console.log(error);
         }
     };
+
+    const getAnswer = (qNumber, answer) => {
+
+        
+        quiz[qNumber].answers.map((answer, i) => {
+            if(answer && (answer.toLowerCase() == quiz[qNumber].correctAnswer.toLowerCase())) document.getElementById(`answer${i}`).classList.add('right-answer');
+            else document.getElementById(`answer${i}`).classList.add('wrong-answer')
+
+        });
+
+        if(qNumber < 10) {
+        const timeOutId = setTimeout(() => {
+            setQNumber(qNumber + 1);
+            quiz[qNumber].answers.map((answer, i) => {
+                document.getElementById(`answer${i}`).classList.remove('right-answer');
+                document.getElementById(`answer${i}`).classList.remove('wrong-answer');
+            });
+            console.log(qNumber);
+            console.log('quizend',quizEnd)
+            if(qNumber == 9) {
+                setQuizEnd(true);
+                console.log('quizend',quizEnd)
+            }
+
+            
+        }, 2000);
+    }
+   
+    }
 
     useEffect(() => {
         getQuiz();
@@ -29,16 +61,29 @@ function QuizType() {
   return (
     <div>
         <h1>{type} Quiz</h1>
-         {(quiz.length > 0) && (
+         {(quiz.length > 0) && !quizEnd && (
             <>
             <h2>{quiz[qNumber].question}</h2> 
             {quiz[qNumber].answers.map((answer, i) => {
-                return <h4 key={i}>{answer}</h4>
+                return <button className='quiz-answer' id={`answer${i}`} onClick={() => getAnswer(qNumber)}>{answer}</button>
             })} 
             </>
            
         )} 
-        <button onClick={() => {setQNumber(qNumber + 1)}}>Next</button>      
+            {quizEnd && <h1>FIM DO QUIZ</h1>}
+            
+         {quizEnd && (
+            <>
+                {quiz.map((question, i) => {
+                   return ( <>
+                    <h2>{question.question}</h2>
+                    <h4>{question.correctAnswer}</h4>
+                    </>
+                   )
+                })}
+            </>
+         )}  
+             
     </div>
   )
 }
