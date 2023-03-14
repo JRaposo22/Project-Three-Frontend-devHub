f0c60c81d6d6f8a603b7457b36d77c26951f1aea
Joao <raposo2281@gmail.com>
2023-03-14 13:09:44 +0000

    adds google logged in

--- src/components/GoogleSigIn.jsx
+++ src/components/GoogleSigIn.jsx
@@ -1,24 +1,47 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useContext} from 'react';
 import axios from 'axios';
+import { AuthContext } from '../context/auth.context';
 import { auth } from "../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
 
+
  function GoogleSigIn()  {
     const [user] = useAuthState(auth);
+    const { authenticateUser } = useContext(AuthContext);
+    const registerGoogle = async () => {  
+      
+      try {
+        const responseUser = await axios.get(`${import.meta.env.VITE_API_URL}/user/${user.email}`);
+        console.log(responseUser)
+        if(responseUser.data[0].email !== user.email){
+          const response = await axios.post(`${import.meta.env.VITE_API_URL}/auth/signup`, {username:user.displayName, email:user.email, password:import.meta.env.VITE_DEFAULT_PASS, adminPass:''});
+        }else{
+          const responseBackend = await axios.post(`${import.meta.env.VITE_API_URL}/auth/login`, {email:user.email, password:import.meta.env.VITE_DEFAULT_PASS});
+          localStorage.setItem('authToken', responseBackend.data.authToken);
+          authenticateUser(); 
+          console.log(responseUser) 
+        }
+                     
+        
+      } catch (error) {
+        console.log(error);
+      }
+        
+    }
     const googleSignIn = () => {
         const provider = new GoogleAuthProvider();
         signInWithRedirect(auth, provider);
-        
-        console.log(auth);
+      
     };
     const signOut = () => {
         auth.signOut();
     };
 
-    const registerGoogle = async () => {
-      const response = await axios.post(`${import.meta.env.VITE_API_URL}/auth/signup`, {username, email, password, adminPass});
-    }
+    useEffect(() => {
+      registerGoogle();
+    }, [user])
+    
   return (
     <>
     <button className="sign-in">
