d6581e5016d90abf3e08a2535537eef38fc17c41
Joao <raposo2281@gmail.com>
2023-03-15 15:47:56 +0000

    quiz css

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import quizService from '../services/quiz.service';
+import './Quiz.css'
 
 function Quiz() {
   return (
     <div> 
-    <h1>All quizes</h1>
+    <h1 className="quizes-title">All quizes</h1>
       <Link to='/quiz/general'>General Quiz</Link>
       <Link to='/quiz/javascript'>Javascritp Quiz</Link>
       <Link to='/quiz/html'>HTML Quiz</Link>
